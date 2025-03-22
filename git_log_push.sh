db63a939327dbdbe252fdfb5c31bf5a12d0e75cb
Tanuj Bhatia <84853581+tanujbhatia24@users.noreply.github.com>
2025-03-23 00:42:27 +0530

    Update git_log_push.sh

--- git_log_push.sh
+++ git_log_push.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 REPO_DIR="$(pwd)"
-LOG_DIR="$REPO_DIR/system_monitor"
-
+LOG_DIR="$REPO_DIR/system_monitor_log"
 
 cd "$REPO_DIR"
-#git add "$LOG_DIR"/*.log
 
 git add .
-git commit -m "Add system monitoring logs - $(date +'%Y-%m-%d %H:%M:%S')"
+git commit -m "Add system monitoring logs"
 git push origin main 
